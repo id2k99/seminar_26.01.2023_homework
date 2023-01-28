05e4d0949ee215d0095077c68098e40a0474b121
sergey <sergey@solaev.ru>
2023-01-28 21:33:57 +0400

    add my homeweork on repository

---
+++ exersice_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход 
+//три числа и выдает максимальное из этих чисел.
+Console.Clear();
+Console.Write(" Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write(" Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write(" Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if(number1 > number2 && number1 > number3) {
+    Console.WriteLine($" {number1} максимальное из трёх чисел");
+}
+
+if(number2 > number3 && number2 > number1) {
+    Console.WriteLine($" {number2} максимальное из трёх чисел");
+}
+
+if(number3 > number1 && number3 > number2) {
+    Console.WriteLine($" {number3} максимальное из трёх чисел");
+}
+
+if (number1 == number2 && number1 == number3 && number3 == number2) {                     //в условии задачи этого нет, 
+    Console.WriteLine(" Все числа равны");                                                //но я добавил данную проверку.
+}
